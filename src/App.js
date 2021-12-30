0cd6ec09398b5b2f010d1f2c42a15898eeeecf74
Dorian Doussain <ddoussain@gmail.com>
2021-12-30 14:25:38 +0300

    Add firebase utils, possibility to create user via email/password method in firestore, enable autorisation (firestore)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+
+import { auth, createUserProfileDoc } from './firebase/firebase.utils';
+
+import { onSnapshot } from "firebase/firestore";
 
 class App extends React.Component {
   constructor() {
@@ -21,8 +24,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ 'currentUser': user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDoc(userAuth);
+
+        onSnapshot(userRef, userDoc => {
+          this.setState({
+            currentUser: {
+              id: userDoc.id,
+              ...userDoc.data()
+            }
+          })
+        });
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, getDoc, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCR_nJl3EdPI6DA2wD9ekXKIY5R1j3SIFk",
@@ -12,10 +12,37 @@ const firebaseConfig = {
   measurementId: "G-33EP82HD95"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-export const auth = getAuth();
-export const store = getFirestore();
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
+
+export const createUserProfileDoc = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = doc(db, 'users', userAuth.uid);
+  const userSnap = await getDoc(userRef);
+
+  if (!userSnap.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(doc(db, 'users', userAuth.uid), {
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  return userRef;
+}
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
