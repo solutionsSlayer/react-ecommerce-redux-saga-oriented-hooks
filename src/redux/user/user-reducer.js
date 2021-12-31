3464a7be38c004ecd571fb1a306a6ba13591edc9
Dorian Doussain <ddoussain@gmail.com>
2021-12-31 10:28:01 +0300

    Setup (redux, reducer, action) of user

---
+++ src/redux/user/user-reducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+  currenUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        currenUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
